4e49db2ab79605f7e554100b6d88b4b351c84e1e
prade <prade@Pradeep-Kumar>
2022-01-07 02:24:20 +0530

    discover server

---
+++ discovery-service/src/test/java/com/buster/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
