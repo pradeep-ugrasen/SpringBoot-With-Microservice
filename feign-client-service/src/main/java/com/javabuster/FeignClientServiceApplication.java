484b209d38bf887d57f0b3d7c08160850c60d4dd
prade <prade@Pradeep-Kumar>
2022-01-07 02:32:12 +0530

    FeignClient Example

---
+++ feign-client-service/src/main/java/com/javabuster/FeignClientServiceApplication.java
@@ -0,0 +1,13 @@
+package com.javabuster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeignClientServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeignClientServiceApplication.class, args);
+	}
+
+}
---
+++ feign-client-service/src/main/java/com/javabuster/client/FeignUserClient.java
@@ -0,0 +1,8 @@
+package com.javabuster.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+
+@FeignClient(url= , name)
+public interface FeignUserClient {
+
+}
